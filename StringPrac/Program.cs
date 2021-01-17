e16531892d48583ae0f4288288f2ef7267d41689
17193 <asialevesquegault@gmail.com>
2021-01-17 13:24:45 -0700

    Add project files.

---
+++ StringPrac/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualBasic;
+
+namespace StringPrac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fullName = "Asia Gault ";
+            Console.WriteLine("Trim: '{0}'", fullName.Trim());//got rid of space at end
+            Console.WriteLine("Trim: '{0}'", fullName.ToUpper());//Each method on string class return new string bec string=immutable
+
+            var index = fullName.IndexOf(' ');
+            var firstName = fullName.Substring(0, index);// up to index of space
+            var lastName = fullName.Substring(index + 1);
+
+            Console.WriteLine("First Name: " + firstName);
+            Console.WriteLine("Last Name: " + lastName);
+
+            // can make splitting easier by using the split method
+
+            var names = fullName.Split(' ');
+            Console.WriteLine("First Name: " + names[0]);
+            Console.WriteLine("Last Name: " +  names[1]);
+            
+            //Replace
+            Console.WriteLine("First Names: "+ fullName.Replace("Asia", "Asia Shalin"));
+            Console.WriteLine("A To Uppercase: "+fullName.Replace("a", "A"));
+
+            //Empty or Null Strings
+            //new version gets rid of empty/whitespace: no need to trim
+            if (String.IsNullOrWhiteSpace(" "))// (String.IsNullOrEmpty(" ".Trim())) trim gets rid of empty space so returns null
+            {
+                Console.WriteLine("Invalid");
+            }
+
+
+        }
+    }
+}
